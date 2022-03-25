11ec9001d634601ceece3eb1939ba6244f7430d2
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-03-25 22:44:39 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+
+const stopBtn = document.querySelector('button[data-stop]');
+
+const handleClickStart = () => {
+    timerId = setInterval(() => {
+        const currentColor = getRandomColor();
+        document.body.style.backgroundColor = currentColor;
+    }, 1000);
+    startBtn.removeEventListener('click', handleClickStart);
+    stopBtn.addEventListener('click', handleClickStop);
+    startBtn.disabled = true;
+};
+
+const handleClickStop = () => {
+    clearInterval(timerId);
+    stopBtn.removeEventListener('click', handleClickStop);
+    startBtn.addEventListener('click', handleClickStart);
+    startBtn.disabled = false;
+};
+
+startBtn.addEventListener('click', handleClickStart);
