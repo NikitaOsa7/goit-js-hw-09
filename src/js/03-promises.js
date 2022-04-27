bdf7e848e2a76c00455e0dedfc9a5a50a38a7f0b
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-04-27 15:53:58 +0300

    повторение

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -16,31 +16,32 @@ function createPromise(position, delay) {
 let promptCounter = 2;
 
 function repeat(delay, step, amount) {
-  setTimeout(() => {
-    createPromise(1, delay);
-    const intervalId = setInterval(() => {
-      if (promptCounter === Number(amount) + 1) {
-        clearInterval(intervalId);
-        return;
-      }
-      createPromise(promptCounter, Number(delay) + (promptCounter - 1) * step);          
-      promptCounter += 1; 
-    }, step);
-  }, delay);
+    setTimeout(() => {
+        createPromise(1, delay)
+        const intervalId = setInterval(() => {
+            if (promptCounter === Number(amount) + 1) {
+                clearInterval(intervalId);
+                return;
+            }
+            createPromise(promptCounter, Number(delay) + (promptCounter - 1) * step);
+            promptCounter += 1
+        }, step);
+    }, delay);
 }
 
-const form = document.querySelector(".form");
+const formEl = document.querySelector(".form");
 
-form.addEventListener("submit", handleSubmit);
+formEl.addEventListener('submit', handleSubmit);
+
 
 function handleSubmit(event) {
-  event.preventDefault();
-  promptCounter = 2;
-  const {
-    elements: { delay, step, amount }
-  } = event.currentTarget;
+    event.preventDefault();
+    promptCounter = 2;
+    const {
+        elements: { delay, step, amount }
+    } = event.currentTarget;
 
-  repeat(delay.value, step.value, amount.value);
+    repeat(delay.value, step.value, amount.value);
 
-  event.currentTarget.reset();
+    event.currentTarget.reset();
 }
